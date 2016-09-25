931a04aacef620b418c4232fea847115d77964e6
Maris <maris2143@gmail.com>
2016-09-25 13:48:30 -0500

    comentario

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Rails_Girls
-Foodie app
+# Foodie
+
+A food experiment for Ruby on Rails workshop
+
+## Overall features
+
+1. Landing page
+2. Featured plates
+3. Plate details (with ingredients)
+4. Vote system (per ingredient)
+
+## Extra Features
+
+1. Pretty UI (bootstrap)
+2. Image Uploads (S3)
+3. Authentication (Devise)
+4. Heroku
+
+## Steps
+[Foodie is coming!](tutos/start.md)
---
+++ app/controllers/plates_controller.rb
@@ -0,0 +1,56 @@
+class PlatesController < ApplicationController
+  before_action :set_plate, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @plates = Plate.all
+  end
+
+  def new
+    @plate= Plate.new
+  end
+
+  def create
+    @plate=Plate.new(plate_params)
+    if @plate.save
+      redirect_to  plates_path, notice: "Plate was successfully created."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @plate.update(plate_params)
+      redirect_to plates_path, notice: "Plate was successfully updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @plate.destroy
+    redirect_to  plates_path, notice: "Plate was successfully destroyed."
+  end
+
+  def show
+  end
+
+  def upvote
+    @plate = Plate.find(params[:id])
+    @plate.votes.create
+    redirect_to(plates_path)
+  end
+
+  private
+
+   def plate_params
+    params.require(:plate).permit(:id,:title,:description,:producer)
+   end
+
+   def set_plate
+     @plate = Plate.find(params[:id])
+   end
+
+end
